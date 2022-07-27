a2e645de85beeb82bc4f0dd60b2411dcc7b8c0fc
Bui Nam <buikhacnam11@gmail.com>
2022-07-27 15:35:15 +0700

    basicNft contract

---
+++ README.md
@@ -0,0 +1,27 @@
+# NFT Playground
+
+Read EIP-721: Non-Fungible Token Standard: https://eips.ethereum.org/EIPS/eip-721
+## Basic NFT
+
+Install dependencies:
+
+```
+yarn add -D dotenv hardhat-deploy @nomiclabs/hardhat-ethers@npm:hardhat-deploy-ethers ethers @nomiclabs/hardhat-waffle hardhat-contract-sizer
+
+```
+
+```
+yarn add @openzeppelin/contracts
+```
+
+OpenZepplin docs: https://docs.openzeppelin.com/contracts/4.x/
+
+
+
+
+
+## Some common commands
+
+```
+yarn hardhat deploy
+```
---
+++ contracts/BasicNft.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.1/contracts/token/ERC721/ERC721.sol
+// import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BasicNft is ERC721 {
+        string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    uint256 private s_tokenCounter;
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    // create new dog -> Call _safeMint()
+    function mintNft() public returns(uint256) {
+        /*
+            _safeMint(address to, uint256 tokenId, bytes memory data)
+                address to: address of the one who calls the function (will be the owner of the token)
+                uint256 tokenId: token id
+                bytes data: token data
+
+        */
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+        return s_tokenCounter;
+    }
+
+    function tokenURI(uint256 /*tokenId*/) public pure override returns(string memory) {
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns(uint256) {
+        return s_tokenCounter;
+    }
+}
