06b0d428d8c924cf360642df8f8e2df79ded5578
Bui Nam <buikhacnam11@gmail.com>
2022-08-02 17:47:52 +0700

    remove logs

--- deploy/04-mint.ts
+++ deploy/04-mint.ts
@@ -10,14 +10,12 @@ const mint: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const basicNft = await ethers.getContract("BasicNft", deployer)
     const basicMintTx = await basicNft.mintNft()
     await basicMintTx.wait(1)
-    console.log(`Basic NFT index 0 tokenURI: ${await basicNft.tokenURI(0)}`)
 
     // Dynamic SVG  NFT
     const highValue = ethers.utils.parseEther("4000")
     const dynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer)
     const dynamicSvgNftMintTx = await dynamicSvgNft.mintNft(highValue)
     await dynamicSvgNftMintTx.wait(1)
-    console.log(`Dynamic SVG NFT index 0 tokenURI: ${await dynamicSvgNft.tokenURI(0)}`)
 
     // Random IPFS NFT
     const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
@@ -37,7 +35,6 @@ const mint: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
             await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address)
         }
     })
-    console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`)
 }
 export default mint
 mint.tags = ["all", "mint"]
